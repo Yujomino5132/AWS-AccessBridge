c4e5e65c80cd6d22030ed89dc1279d4f9f25f3d9
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-07-06 18:00:57 +0000

    FEAT: save the user email in context

--- src/endpoints/IActivityAPIRoute.ts
+++ src/endpoints/IActivityAPIRoute.ts
@@ -1,15 +1,19 @@
 import { OpenAPIRoute } from 'chanfana';
 import { Context } from 'hono';
-import { DefaultInternalServerError, InternalServerError, IServiceError } from '../error';
+import { DefaultInternalServerError, InternalServerError, IServiceError, UnauthorizedError } from '../error';
 
 abstract class IActivityAPIRoute<TRequest extends IRequest, TResponse extends IResponse, TEnv extends IEnv> extends OpenAPIRoute {
   async handle(c: Context<TEnv>) {
     try {
-      const body = await c.req.json();
-      const request = body as TRequest;
+      const userEmail: string | undefined = c.req.header('Cf-Access-Authenticated-User-Email');
+      if (!userEmail) {
+        throw new UnauthorizedError('No authenticated user email provided in request headers.');
+      }
+      c.set('AuthenticatedUserEmailAddress', userEmail);
 
-      const response = await this.handleRequest(request, c.env as TEnv);
-
+      const body: unknown = await c.req.json();
+      const request: TRequest = body as TRequest;
+      const response: TResponse = await this.handleRequest(request, c.env as TEnv);
       return c.json(response);
     } catch (error: unknown) {
       if (!(error instanceof IServiceError) || error instanceof InternalServerError) {
@@ -28,6 +32,10 @@ abstract class IActivityAPIRoute<TRequest extends IRequest, TResponse extends IR
   }
 
   protected abstract handleRequest(request: TRequest, env: TEnv): Promise<TResponse>;
+
+  protected getAuthenticatedUserEmailAddress(c: Context<TEnv>):string{
+    return c.get('AuthenticatedUserEmailAddress');
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
@@ -36,8 +44,11 @@ interface IRequest {}
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface IResponse {}
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface IEnv {}
+interface IEnv {
+  Variables: {
+    AuthenticatedUserEmailAddress: string;
+  };
+}
 
 export { IActivityAPIRoute };
 export type { IRequest, IResponse, IEnv };
