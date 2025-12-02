c15fd20ddcb71e5651288f73301fe95752fc54ee
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-12-02 00:58:30 +0000

    FEAT: reject internal requests without hmac signature

--- src/middleware/MiddlewareHandlers.ts
+++ src/middleware/MiddlewareHandlers.ts
@@ -1,12 +1,13 @@
-import { INTERNAL_SIGNATURE_HEADER } from '@/constants';
+import { INTERNAL_HEADER_PREFIX, SELF_WORKER_BASE_HOSTNAME } from '@/constants';
 import { Context, Next } from 'hono';
 import { HMACHandler } from './HMACHandler';
 
 class MiddlewareHandlers {
   public static hmacValidation() {
     return async (c: Context<{ Bindings: Env }>, next: Next): Promise<void> => {
-      const signature: string | undefined = c.req.header(INTERNAL_SIGNATURE_HEADER);
-      if (signature) {
+      const url: URL = new URL(c.req.url);
+      const hasInternalHeaders: boolean = Array.from(c.req.raw.headers.keys()).some((key) => key.startsWith(INTERNAL_HEADER_PREFIX));
+      if (url.hostname === SELF_WORKER_BASE_HOSTNAME || hasInternalHeaders) {
         await HMACHandler.validateInternalRequest(c, next);
       } else {
         await next();
